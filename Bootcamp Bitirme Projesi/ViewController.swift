37a3f00e2500fab982b90db372bd3c130d3eb065
Joshua-U-Sands <joshua.uzay@gmail.com>
2022-09-28 14:04:59 +0300

    Initial Commit

---
+++ Bootcamp Bitirme Projesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bootcamp Bitirme Projesi
+//
+//  Created by Joshua Sands on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
