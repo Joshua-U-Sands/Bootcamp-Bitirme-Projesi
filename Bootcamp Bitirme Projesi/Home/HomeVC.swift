fdcca96e99595816d4ebae8a2aa37256720f1b18
Joshua-U-Sands <joshua.uzay@gmail.com>
2022-10-07 17:26:48 +0300

    Main page design complete

--- Bootcamp Bitirme Projesi/Home/HomeVC.swift
+++ Bootcamp Bitirme Projesi/Home/HomeVC.swift
@@ -12,13 +12,45 @@ class HomeVC: UIViewController {
     //Views
     @IBOutlet weak var tabBar: UIView!
     @IBOutlet weak var homeBarView: UIView!
+    @IBOutlet weak var homeImageview: UIImageView!
+    
+    @IBOutlet weak var flightsButton: UIButton!
+    @IBOutlet weak var hotelsButton: UIButton!
+   
+    @IBOutlet weak var topPickCollection: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        topPickCollection.isUserInteractionEnabled = true
+        
+        self.view.backgroundColor = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+        
         //Bar Edit
         homeBarView.layer.cornerRadius = homeBarView.frame.size.height / 2
         homeBarView.backgroundColor = UIColor.orange
+        
+        topPickCollection.dataSource = self
+        topPickCollection.delegate = self
+        
+       
+
+        topPickCollection.layer.shadowColor = UIColor(red: 0.094, green: 0.196, blue: 0.451, alpha: 0.1).cgColor
+
+        topPickCollection.layer.shadowOpacity = 1
+
+        topPickCollection.layer.shadowRadius = 16
+
+        topPickCollection.layer.shadowOffset = CGSize(width: 2, height: 8)
+
+        
+        topPickCollection.layer.shadowColor = UIColor(red: 0.094, green: 0.196, blue: 0.451, alpha: 0.1).cgColor
+
+        topPickCollection.layer.shadowOpacity = 1
+
+        topPickCollection.layer.shadowRadius = 16
+
+        topPickCollection.layer.shadowOffset = CGSize(width: 2, height: 8)
     }
     
 //    func customTabBar(){
@@ -36,6 +68,25 @@ class HomeVC: UIViewController {
         vc.modalPresentationStyle = .fullScreen
         present(vc,animated: false)
     }
+    
+}
+
+extension HomeVC: UICollectionViewDelegate{
+    
+}
+
+extension HomeVC: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = topPickCollection.dequeueReusableCell(withReuseIdentifier: "customCollectionCell", for: indexPath)
+        return cell
+        
+    }
+    
     
 }
 
---
+++ Bootcamp Bitirme Projesi/Home/topPickCell.swift
@@ -0,0 +1,12 @@
+//
+//  topPickCell.swift
+//  Bootcamp Bitirme Projesi
+//
+//  Created by Joshua Sands on 7.10.2022.
+//
+
+import UIKit
+
+class topPickCell: UICollectionViewCell {
+    
+}
